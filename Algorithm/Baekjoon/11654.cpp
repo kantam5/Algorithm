cf39c177294f54cef5d6a9b29bc6b74d74b5c962
kantam5 <wlsry45246@gmail.com>
2022-07-10 18:41:11 +0900

    백준 11654 아스키 코드

---
+++ Algorithm/Baekjoon/11654.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int result = 0;
+
+    char latter;
+    cin >> latter;
+
+    result = (int)latter;
+
+    cout << result;
+
+    return 0;
+}
