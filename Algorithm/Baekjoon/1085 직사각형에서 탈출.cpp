78a6d63e6852f32338c60edb098e9f22e2431a65
kantam5 <wlsry45246@gmail.com>
2022-07-16 23:28:59 +0900

    백준 1085 직사각형에서 탈출

---
+++ Algorithm/Baekjoon/1085 직사각형에서 탈출.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+
+    int w;
+    int h;
+
+    cin >> x >> y >> w >> h;
+
+    cout << min(min(x, y), min((w - x), (h - y)));
+
+    return 0;
+}
