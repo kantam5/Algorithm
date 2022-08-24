9d5eaea544ae690863edc4ff80de847a472c667c
kantam5 <wlsry45246@gmail.com>
2022-08-24 23:23:11 +0900

    백준 11726 2Xn 타일링

---
+++ Algorithm/Baekjoon/11726 2Xn 타일링.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <memory.h>
+
+using namespace std;
+
+int cache[1001];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cache[0] = 1;
+    cache[1] = 2;
+
+    for (int i = 2; i < n; i++)
+    {
+        cache[i] = (cache[i - 1] + cache[i - 2]) % 10007;
+    }
+
+    cout << cache[n - 1];
+
+    return 0;
+}
