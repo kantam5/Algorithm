4208db9e3b190b058af51e7f8013bb9d1f7b7428
kantam5 <wlsry45246@gmail.com>
2022-08-28 20:35:56 +0900

    백준 11723 집합 비트마스킹

--- Algorithm/Baekjoon/11723 집합.cpp
+++ Algorithm/Baekjoon/11723 집합.cpp
@@ -70,3 +70,82 @@ int main(void)
 
 	return 0;
 }
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <memory.h>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int M;
+	cin >> M;
+
+	int S = 0;
+
+	while (M--)
+	{
+		string command;
+		cin >> command;
+
+		if (command == "add")
+		{
+			int temp;
+			cin >> temp;
+			// 비트 or 연산자
+			S |= (1 << temp);
+		}
+		else if (command == "remove")
+		{
+			int temp;
+			cin >> temp;
+			// 비트 and 연산자
+			S &= ~(1 << temp);
+		}
+		else if (command == "check")
+		{
+			int temp;
+			cin >> temp;
+			// 비트 and 연산자로 해당자리가 1인지 확인
+			if (S & (1 << temp))
+			{
+				cout << 1 << '\n';
+			}
+			else
+			{
+				cout << 0 << '\n';
+			}
+		}
+		else if (command == "toggle")
+		{
+			int temp;
+			cin >> temp;
+			// 비트 xor 연산자
+			S ^= (1 << temp);
+		}
+		else if (command == "all")
+		{
+			// 1000000000000000000000 - 1 = 11111111111111111111
+			S = (1 << 21) - 1;
+		}
+		else if (command == "empty")
+		{
+			// 00000000000000000000
+			S = 0;
+		}
+	}
+
+	return 0;
+}
