b2ea9f78d089f7cfcc1a3359c5c7f283186b9b43
Jingyo_Jeong <59176502+kantam5@users.noreply.github.com>
2023-09-16 13:15:35 +0900

    16953 A to B 업데이트

--- Algorithm/Baekjoon/너비 우선 탐색/16953 A to B.cpp
+++ Algorithm/Baekjoon/너비 우선 탐색/16953 A to B.cpp
@@ -67,3 +67,35 @@ int main()
 
     return 0;
 }
+
+// 다른 사람 꺼
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int mini = 10000; // 임의의 큰수
+long long A, B, cnt;
+
+void dfs(long long a, int cnt)
+{
+    if (a > B) return;
+
+    if (a == B)
+    {
+        mini = min(mini, cnt);
+    }
+    dfs(a * 2, cnt + 1);
+    dfs(a * 10 + 1, cnt + 1);
+
+}
+
+int main()
+{
+    cin >> A >> B;
+
+    dfs(A, 1);
+
+    if (mini == 10000) cout << -1;
+    else cout << mini;
+}
